597d621ea6a92b95d284d6a6e980cbb80678705c
Jailton Domingos <development.j@Outlook.com>
2021-12-16 17:32:34 -0300

    [JavaScript] relogio.js

---
+++ relogio.js
@@ -0,0 +1,27 @@
+function Relogio() {
+    this.hora = 0
+    this.minuto = 0
+    this.segundo = 0
+
+    const self = this
+    setInterval(function () {
+        this.segundo++
+        
+        if (this.segundo == 60) { // Setar valores menores para ver o funcionamento de forma mais rapida
+            this.segundo = 0
+            this.minuto++
+            if (this.minuto == 60) {// Setar valores menores para ver o funcionamento de forma mais rapida
+                this.minuto = 0
+                this.hora++
+                if (this.hora == 24) {// Setar valores menores para ver o funcionamento de forma mais rapida
+                    this.hora = 0
+                }
+            }
+        }
+
+        console.log(`${this.hora} : ${this.minuto} : ${this.segundo}`)
+    }.bind(this), 1000)
+
+}
+
+new Relogio
