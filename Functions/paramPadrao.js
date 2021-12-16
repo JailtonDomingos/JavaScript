d74ade0cd18dc1b2c3b65f5c7884f7fef7802fe8
Jailton Domingos <development.j@Outlook.com>
2021-12-16 15:32:10 -0300

    [JavaScript] paramPadrao.js

--- Functions/paramPadrao.js
+++ Functions/paramPadrao.js
@@ -13,4 +13,10 @@ function soma2(a ,b ,c) {
     c = isNaN(c) ? 1 : c
     return a + b + c
 }
-console.log(soma2(1, 4, 'jailton'))
+console.log(soma2(), soma2(3), soma2(1, 4, 'jailton'))
+
+// Valor padrão do ES2015(ES6)
+function soma3(a = 1, b = 1, c = 1){
+    return a + b + c
+}
+console.log(soma3(), soma3(2,2), soma3(5,5,5))
