6d083f0110b7c677c7dc2d1e466af838138cbe54
Jailton Domingos <development.j@Outlook.com>
2021-12-17 15:08:04 -0300

    [JavaScript] FIX - arrowFunction.js

--- Functions/arrowFunction.js
+++ Functions/arrowFunction.js
@@ -1,15 +1,19 @@
-let dobro1 = function (a){ // Function normal
+dobro1 = (a) => { // Arrow funtionc com parenteses e com bloco para adicionar mais de uma linha
     return 2 * a
 }
 
-dobro2 = (a) => { // Arrow funtionc com parenteses e com bloco para adicionar mais de uma linha
-    return 2 * a
-}
-
-dobro3 = a => 2 * a // Arrow com somente 1 linha(Removido o parenteses e return abstraido)
+dobro2 = a => 2 * a // Arrow com somente 1 linha(Removido o parenteses e return abstraido)
 
 console.log(dobro1(1))
 console.log(dobro2(2))
-console.log(dobro3(3))
 
-//console.log(soma () => 9 + 5)
+function Pessoa(){
+    this.idade = 0
+
+    setInterval(() => {
+        this.idade++
+        console.log(this.idade)
+    }, 1000);
+}
+
+new Pessoa
