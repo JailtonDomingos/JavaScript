1fec205ac6df52234f35a9929a3aa05c20663327
Jailton Domingos <development.j@Outlook.com>
2021-12-21 18:40:19 -0300

    [JavaScript] IIFE.js class

---
+++ Functions/IIFE.js
@@ -0,0 +1,6 @@
+// IIFE -> Immediately Invoked Function Expression
+
+(function () {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})() // Declarar dentro de parenteses e colocar esse par de parenteses no final, faz com que ela já seja executada
