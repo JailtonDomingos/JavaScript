a9b842f17d866ebfa381438ad304372fc18aa097
Jailton Domingos <development.j@Outlook.com>
2021-12-22 18:34:45 -0300

    [JavaScript] ex01.js class - 02

---
+++ Projetos/Exercicios/ex01.js
@@ -0,0 +1,10 @@
+/* 
+Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.
+*/
+
+function soma (x ,y) {
+    return console.log(`Soma: ${x + y}, Subtração: ${x - y}, Multiplicação: ${x * y}, Divisão: ${x / y}`)
+}
+
+soma(2,2)
