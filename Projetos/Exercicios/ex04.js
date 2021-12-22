1ba8b5912024eaf30870539ed71bd054de782171
Jailton Domingos <development.j@Outlook.com>
2021-12-22 18:48:41 -0300

    [JavaScript] ex04.js class

---
+++ Projetos/Exercicios/ex04.js
@@ -0,0 +1,11 @@
+/*
+Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.
+*/
+
+function div(x, y) {
+    return console.log(x%y)
+}
+
+div(10, 3)
+div(10, 2)
