ca7a80a8bac52fe1e22ee36a4ab20eaf79ec2d16
Jailton Domingos <development.j@Outlook.com>
2021-12-22 21:47:35 -0300

    [JavaScript] ex10-multiploPor3.js class

---
+++ Projetos/Exercicios/ex10-multiploPor3.js
@@ -0,0 +1,8 @@
+/*
+Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
+ou false
+*/
+
+let verifica = param => param % 3 == 0 ? true : false 
+
+console.log(verifica(3))
