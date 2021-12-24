d44b4cb0e45f9953ce3541645c61141c5a66266e
Jailton Domingos <development.j@Outlook.com>
2021-12-23 21:34:21 -0300

    [JavaScript] ex16-calculadora.js class

---
+++ Projetos/Exercicios/ex16-calculadora.js
@@ -0,0 +1,30 @@
+/*
+ Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebe
+como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores
+*/
+
+function calculadora(x, y, operacao) {
+    switch (operacao) {
+        case 'soma':
+            console.log(x + y)
+            break;
+        case 'subtracao':
+            console.log(x - y)
+            break;
+        case 'divisao':
+            console.log(x / y)
+            break;
+        case 'multiplicacao':
+            console.log(x * y)
+            break;
+    
+        default:
+            console.log('Houve um erro ao identificar a operação, por favor digite novamente!')
+            break;
+    }
+}
+
+calculadora(2, 2, 'soma')
+calculadora(5, 3, 'subtracao')
+calculadora(3, 3, 'divisao')
+calculadora(3, 3, 'multiplicacao')
