22841937fdc2da44edcbe79cdf3c51507ed1b7ab
Jailton Domingos <development.j@outlook.com>
2021-12-25 19:38:31 -0300

    Update ex5-formataMoeda.js

--- Projetos/Exercicios/ex5-formataMoeda.js
+++ Projetos/Exercicios/ex5-formataMoeda.js
@@ -6,6 +6,9 @@ um exercício simples para mostrar dinheiro sempre da forma correta. Desenvolva 
 que ela receba um valor como 0.30000000000000004 e retorne R$0,30 (observe a vírgula e o ponto).
 */
 
+
+// Exemplo em arrow function
+// const formataMoeda = param => { return param. toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
 function formataMoeda(x) {
     return x.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 }
