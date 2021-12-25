ac803fd5d865c93be4bb28963061cae53a121ce4
Jailton Domingos <development.j@outlook.com>
2021-12-25 19:31:46 -0300

    Update ex3-exponencial.js

--- Projetos/Exercicios/ex3-exponencial.js
+++ Projetos/Exercicios/ex3-exponencial.js
@@ -1,7 +1,10 @@
 // Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
 
+// Exemplo de arrow armazenado em variável 
+//const potencia = (x, y) => return x**y
 function exp (x, y) {
     return x**y
 }
 
+//console.log(potencia(2, 3))
 console.log(exp(2, 3))
