ae5deb01e6df680f3532862eec5626dded43c9e4
Jailton Domingos <development.j@outlook.com>
2021-12-25 19:34:55 -0300

    Update ex4-divisorDividendo.js

--- Projetos/Exercicios/ex4-divisorDividendo.js
+++ Projetos/Exercicios/ex4-divisorDividendo.js
@@ -3,9 +3,12 @@ Crie uma função que irá receber dois valores, o dividendo e o divisor. A fun�
 e o resto da divisão destes dois valores.
 */
 
+// Exemplo arrow function armazenada
+// const divisao = (x, y) => return x%y
 function div(x, y) {
     return x%y
 }
 
+//console.log(divisao(10, 3))
 console.log(div(10, 3))
 console.log(div(10, 2))
