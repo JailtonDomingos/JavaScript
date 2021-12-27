add5f7cfbf258c6e27269fa01a6def5b9e3100ac
Jailton Domingos <development.j@Outlook.com>
2021-12-27 18:15:10 -0300

    [JavaScript] HackerRank 10 Days JS - day04-2.js class

---
+++ Projetos/HackerRank-10DaysOfJavaScript/Day04/day04-2.js
@@ -0,0 +1,55 @@
+/* 
+Task
+
+Complete the function in the editor. It has one parameter: an array, 'a', of objects. 
+Each object in the array has two integer properties denoted by 'x' and 'y'. The function must return a count of all such objects  in array  that satisfy 'o.x == o.y'.
+
+Input Format
+
+The first line contains an integer denoting .
+Each of the  subsequent lines contains two space-separated integers describing the values of  and .
+*/
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+
+const arrayObjects = [
+    object1 = {
+        x: 1,
+        y: 1
+    },
+    object2 = {
+        x: 2,
+        y: 3
+    },
+    object3 = {
+        x: 3,
+        y: 3
+    },
+    object4 = {
+        x: 3,
+        y: 4
+    },
+    object5 = {
+        x: 4,
+        y: 5
+    },
+]
+
+function getCount(arrayObjects) {
+    let variavelControle = 0
+    for (let i = 0; i < arrayObjects.length; i++) {
+        if (arrayObjects[i].y === arrayObjects[i].x) {
+            variavelControle++
+        }
+        
+    }
+
+    return variavelControle
+}
+
+console.log(getCount(arrayObjects))
