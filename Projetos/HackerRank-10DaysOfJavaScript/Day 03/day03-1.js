cb267061f6766b06d719f4c896e838935325b708
Jailton Domingos <development.j@Outlook.com>
2021-12-26 22:00:20 -0300

    [JavaScript] HackerRank 10 Days JS - day03-1.js class (FIX)

--- Projetos/HackerRank-10DaysOfJavaScript/Day 03/day03-1.js
+++ Projetos/HackerRank-10DaysOfJavaScript/Day 03/day03-1.js
@@ -11,16 +11,18 @@ int: the second largest number in
 function getSecondLargest(nums) {
     // Complete the function
     let maior = 0, segundoMaior = 0
-    for (let i = 0; i < nums.length; i++) {
-        
-        if (nums[i] >= maior) {
+    maior = nums[0]
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > maior) {
             segundoMaior = maior
             maior = nums[i]
+        } else if ((nums[i] != maior) && (nums[i] > segundoMaior)) {
+            segundoMaior = nums[i]
         }
     }
 
     return segundoMaior
 }
 
-const nums = [5, 7, 4, 2, 6, 15, 24, 12, 90, 0]
+const nums = [2, 3, 6, 6, 5]
 console.log(getSecondLargest(nums))
