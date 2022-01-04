de85fd431a192554923a97bde01c58925bb07dc6
Jailton Domingos <development.j@Outlook.com>
2022-01-03 21:20:00 -0300

    [JavaScript] ex28-vetorParImpar.js class

---
+++ Projetos/Exercicios/ex28-vetorParImpar.js
@@ -0,0 +1,21 @@
+/*
+Ler um vetor de números inteiros e imprimir quantos são pares e quantos são ímpares.
+*/
+
+function lerArray (paramArray) {
+    let impar = 0
+    let par = 0
+
+    for (let i = 0; i < paramArray.length; i++) {
+        if (i % 2 === 0) {
+            par++
+        } else {
+            impar++
+        }    
+    }
+
+    console.log(`Quantidade de números impares: ${impar} / Quantidade de números pares: ${par}`)
+
+}
+
+lerArray([1, 2, 3, 4, 5, 6, 7, 8, 9])
