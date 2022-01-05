9419e54f9b582e149d535fd1d511680693762b68
Jailton Domingos <development.j@outlook.com>
2022-01-04 23:32:12 -0300

    Update ex29-verificaIntervalo.js

--- Projetos/Exercicios/ex29-verificaIntervalo.js
+++ Projetos/Exercicios/ex29-verificaIntervalo.js
@@ -5,7 +5,7 @@ deles estão fora do intervalo, escrevendo estas informações.
 */
 
 function noIntervalo(param, min, max) {
-    return (( param - min ) * ( param - max)  <= 0 );
+    return (( param - min ) * ( param - max)  <= 0 ); // Lógica para verificar se está no intervalo, pois se não estiver o valor sempre será maior que ZERO, caso contrário return true, ou seja, está no intervalo.
 }
 
 let nIntervaloTrue = 0
@@ -14,7 +14,7 @@ let nIntervaloFalse = 0
 let param = [1, 2, 3, 10, 11, 19, 20, 21, 22, 23]
 
 for (let i = 0; i < param.length; i++) {
-    if (noIntervalo(param[i], 10, 20)) {
+    if (noIntervalo(param[i], 10, 20)) { // Verificação já recebendo o Boolean da function noIntervalo
         nIntervaloTrue++
     } else {
         nIntervaloFalse++
