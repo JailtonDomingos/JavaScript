dbd8b078edc8af5da1ce7eb99a0c7bc7b30bd509
Jailton Domingos <development.j@outlook.com>
2022-01-04 23:34:09 -0300

    Update ex25-paresZeroToOneHundred.js

--- Projetos/Exercicios/ex25-paresZeroToOneHundred.js
+++ Projetos/Exercicios/ex25-paresZeroToOneHundred.js
@@ -3,8 +3,8 @@ Fazer um programa para encontrar todos os pares entre 1 e 100.
 */
 
 function encontraPares () {
-    for (let i = 0; i < 100; i++) {
-        if (i % 2 === 0) {
+    for (let i = 0; i < 100; i++) { // Percorrendo até 100
+        if (i % 2 === 0) { // Operação verificando o resto
             console.log(i)
         }
     }
